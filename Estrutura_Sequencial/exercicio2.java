ff6a37f3665fc4129fb6d6d7951be95af2fcb8f9
Allan Utxavo <allanutxavo@gmail.com>
2023-09-14 15:59:11 +0200

    Organizando a estrutura do miniTeste

--- Estrutura_Sequencial/exercicio2.java
+++ Estrutura_Sequencial/exercicio2.java
@@ -1,5 +1,19 @@
 package Paradigmas.Estrutura_Sequencial;
-
+import java.util.Scanner;
 public class exercicio2 {
+    public static void main(String[] args) {
 
+            Scanner a = new Scanner(System.in);
+
+        double pi = 3.14159, raio , area;
+        System.out.println("Digite a area  do circulo: ");
+
+         raio = a.nextDouble();
+
+         area = pi * Math.pow(raio, 2);
+
+        System.out.printf("A area do circulo e: %.4f%n", area);
+
+        a.close();
+    }
 }
